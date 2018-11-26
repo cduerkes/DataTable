bedee0fc4f0b67838d63034eddcf234345bea9cb
Christopher Duerkes <christopher.duerkes@gmail.com>
2018-11-26 00:46:57 -0500

    Display hover data and update organization and styling.

--- js/index.js
+++ js/index.js
@@ -1,34 +1,58 @@
-function filterColumn ( i ) {
-    $('#test').DataTable().column( i ).search(
-        $('#col'+i+'_filter').val(),
-        $('#col'+i+'_regex').prop('checked')
-    ).draw();
-}
-
 $(document).ready(function() {
-    $('#body').DataTable();
+    // Don't initially display main table title metadata div
+    $('.tableTitle, .metaDiv').hide();
+
+    // Initiate main table template
+    $('#main-template').DataTable();
+
+    // Call parse data function when file uploaded
     $("#csv-file").change(parseData);
 
+    // Detect scroll position to style sticky metadata div
+    $(document).scroll(function() {
+      var y = $(this).scrollTop();
+
+      if (y > 300) {
+        $('.metaDiv').fadeIn();
+        $('.metaDiv').css({
+            "position": "fixed", 
+            "top": "39px",
+            "width": "50%",
+            "height": "50px"
+        });
+      } else {
+        $('.metaDiv').css({
+            "position": "static", 
+            "margin": "0 0 3rem", 
+            "width": "50%",
+            "height": "35px"
+        });
+      }
+    });
+
+    // Set up event listener on input fields to filter corresponding columns
     $('body').on( 'keyup click', 'input.column_filter', function () {
         filterColumn( $(this).parents('tr').attr('data-column') );
     });
 
+    // Set up event listener on table rows to get and display visible table row
     $('body').on( 'mouseenter', 'tr', function () {
-        const table = $('#test').DataTable();
+        const table = $('#main').DataTable();
 
         var rowNumber = table.rows( { order: 'applied' } ).nodes().indexOf( this );
-
-
-        console.log(`row index: ${rowNumber + 1}`);
+        if (rowNumber >= 0) {
+            $('.metaDiv').text(`Current row: ${rowNumber + 1}`);
+        }
     });
 
+    // Set up event listener on table cells to eventually display of column type when hovering over column names
+    // Question 6 referred to a 'column type'
     $('body').on( 'mouseenter', 'td', function () {
-        const table = $('#test').DataTable();
+        const table = $('#main').DataTable();
         const data = table.cell( this ).data();
 
+    // Detect data types
         if (typeof(data) !== 'undefined') {
-            console.log(`row index: ${table.row( this ).index()}`);
-
             if (data[0] ==='$') {
                 console.log('currency');
             } else if (data.toString().indexOf("/") > -1) {
@@ -41,31 +65,30 @@ $(document).ready(function() {
         }
     });
 
-    $('body').on( 'mouseenter', '#test th', function () {
-        const table = $('#test').DataTable();
+    // Set up event listener on table heading to get and display column sort method and search term, if applied
+    $('body').on( 'mouseenter', '#main th', function () {
+        const table = $('#main').DataTable();
         let sort = $(this).attr("aria-sort");
         let search = table.column(this).search();
 
         if (search !== '') {
-            console.log(`search by: ${search}`);
+            $('.metaDiv').text(`Search by: ${search}`);
         }
-        if(typeof sort !== "undefined") {
-            console.log(sort.charAt(0).toUpperCase() + sort.slice(1).toLowerCase());
+
+        if (typeof sort !== "undefined") {
+            if (search !== '') {
+                $('.metaDiv').text(`Search by: ${search}, Sort: ${sort.charAt(0).toUpperCase() + sort.slice(1).toLowerCase()}`);
+            } else {
+                $('.metaDiv').text(`Sort: ${sort.charAt(0).toUpperCase() + sort.slice(1).toLowerCase()}`);
+            }
+
         } else {
             console.log("Unsorted");
         }
     });
 });
 
-function isDate(str) {    
-    const params = str.split(/[\.\-\/]/);
-    const yyyy = parseInt(params[0],10);
-    const mm   = parseInt(params[1],10);
-    const dd   = parseInt(params[2],10);
-    const date = new Date(yyyy,mm-1,dd,0,0,0,0);
-    return mm === (date.getMonth()+1) && dd === date.getDate() && yyyy === date.getFullYear();
-}
-
+// Function to parse csv file and then render html strings and update index column
 function parseData(e) {
     const file = e.target.files[0];
 
@@ -75,42 +98,62 @@ function parseData(e) {
             renderSearch(results.data);
             renderTable(results.data);
             getRowIndex();
+            $('.tableTitle, .metaDiv').show();
         }
     });
 }
 
-function renderTable(data) {
-    const header = getTableHeader(data);
-    const body = getTableBody(data);
-    
-    const table = `
-    <table id="test" class="display" style="width:100%">
-        <thead>${header}</thead>
-        <tbody>${body}<tbody>
-    </table>`;
-
-    $('header').hide();
-    $('#body').parent().replaceWith(table);
-    $('#test').DataTable({
-        "columnDefs": [{
-            "targets": "_all",
-            "createdCell": function (td, cellData, rowData, row, col) {
-                $(td).attr('data-title', "your cell title");
-            }
-        }]
-    });
+// Function to build search table header
+function getSearchHeader() {
+    const header = `
+        <tr>
+            <th>Target</th>
+            <th>Search text</th>
+            <th>Treat as regex</th>
+        </tr>
+    `;
+    return header;
 }
 
-function getRowIndex() {
-    const table = $('#test').DataTable();
-    
-    table.rows().every( function ( rowIdx, tableLoop, rowLoop ) {
-        const data = this.data();
-        data[0] = rowIdx + 1;
-        this.data(data);
-    });
+// Function to build search table body
+function getSearchBody(data) {
+    let rows = ``;
+
+    for (let i = 0; i < data[0].length; i++) {
+        rows += `<tr id="filter_col${i + 1}" data-column="${i + 1}">`;
+        rows += `<td>Column - ${data[0][i]}</td>`;
+        rows += `<td align="center"><input type="text" class="column_filter" id="col${i + 1}_filter"></td>`;
+        rows += `<td align="center"><input type="checkbox" class="column_filter" id="col${i + 1}_regex"></td>`;
+        rows += `</tr>`;
+    }
+
+    return rows;
 }
 
+// Function to replace template html with search table
+function renderSearch(data) {
+    const sheader = getSearchHeader();
+    const sbody = getSearchBody(data);
+    
+    const searchTable = `
+    <table cellpadding="3" cellspacing="0" border="0" style="width: 50%; margin: 0 auto 2em auto;">
+        <thead>${sheader}</thead>
+        <tbody>${sbody}<tbody>
+    </table>
+    `;
+
+    $('#search-template').replaceWith(searchTable);
+}
+
+// Function to filter table data using input values/regular expressions
+function filterColumn ( i ) {
+    $('#main').DataTable().column( i ).search(
+        $('#col'+i+'_filter').val(),
+        $('#col'+i+'_regex').prop('checked')
+    ).draw();
+}
+
+// Function to build main table header
 function getTableHeader(data) {
     let rows = `<tr>`;
         rows += `<th>Index</th>`;
@@ -123,6 +166,7 @@ function getTableHeader(data) {
     return rows;
 }
 
+// Function to build main table body
 function getTableBody(data) {
     let rows = '';
 
@@ -140,41 +184,46 @@ function getTableBody(data) {
     return rows;
 }
 
-function renderSearch(data) {
-    const sheader = getSearchHeader();
-    const sbody = getSearchBody(data);
+// Function to replace template html with data table
+function renderTable(data) {
+    const header = getTableHeader(data);
+    const body = getTableBody(data);
     
-    const searchTable = `
-    <table id="stest" cellpadding="3" cellspacing="0" border="0" style="width: 50%; margin: 0 auto 2em auto;">
-        <thead>${sheader}</thead>
-        <tbody>${sbody}<tbody>
-    </table>
-    `;
+    const table = `
+    <table id="main" class="display" style="width:100%">
+        <thead>${header}</thead>
+        <tbody>${body}<tbody>
+    </table>`;
 
-    $('#search').replaceWith(searchTable);
+    $('header').hide();
+    $('#main-template').parent().replaceWith(table);
+    $('#main').DataTable({
+        fixedHeader: true,
+        "pageLength": 50
+    });
 }
 
-function getSearchHeader() {
-    const header = `
-        <tr>
-            <th>Target</th>
-            <th>Search text</th>
-            <th>Treat as regex</th>
-        </tr>
-    `;
-    return header;
+// Function to update index column with original row number
+function getRowIndex() {
+    const table = $('#main').DataTable();
+    
+    table.rows().every( function ( rowIdx, tableLoop, rowLoop ) {
+        const data = this.data();
+        data[0] = rowIdx + 1;
+        this.data(data);
+    });
 }
 
-function getSearchBody(data) {
-    let rows = ``;
-
-    for (let i = 0; i < data[0].length; i++) {
-        rows += `<tr id="filter_col${i + 1}" data-column="${i + 1}">`;
-        rows += `<td>Column - ${data[0][i]}</td>`;
-        rows += `<td align="center"><input type="text" class="column_filter" id="col${i + 1}_filter"></td>`;
-        rows += `<td align="center"><input type="checkbox" class="column_filter" id="col${i + 1}_regex"></td>`;
-        rows += `</tr>`;
-    }
-
-    return rows;
+// Function to identify whether string is a date
+function isDate(str) {
+    const params = str.split(/[\.\-\/]/);
+    const yyyy = parseInt(params[0],10);
+    const mm   = parseInt(params[1],10);
+    const dd   = parseInt(params[2],10);
+    const date = new Date(yyyy,mm-1,dd,0,0,0,0);
+    return mm === (date.getMonth()+1) && dd === date.getDate() && yyyy === date.getFullYear();
 }
+
+
+
+
